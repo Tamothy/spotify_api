ad4b1c8337a7118b839f3432c630942a6aa890a7
Megan Kaluza and Tam Nguyen <megan+tam@>
2016-11-01 11:49:24 -0700

    initial commit

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -12,9 +12,9 @@ var request = require('request'); // "Request" library
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
-var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+var client_id = ''; // Your client id
+var client_secret = ''; // Your secret
+var redirect_uri = 'http://spotify.com/callback/'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
